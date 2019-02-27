4a3d79765de76001b1d2dbc58cb915b6200dbfaa
Raiondesu <a.raiondesu@gmail.com>
2019-02-27 12:36:49 +0300

    Update [component]: better persist the winner

--- component.js
+++ component.js
@@ -8,18 +8,18 @@ export default {
     name: String
   },
   render(h, ctx) {
-    const name = ctx.props.name || ctx.parent.$options.name
-    if (!name) throw 'VueA2B Error: The test name is mandatory!'
+    const name = ctx.props.name || ctx.parent.$options.name;
+    if (!name) throw 'VueA2B Error: The test name is mandatory!';
 
-    const variations = ctx.slots()
-    const winner = ctx.props.always ||
-      storage.entry[name] ||
-      randomCandidate(ctx.children)
-
-    storage.entry = {
-      name,
-      winner
+    if (!storage.entry[name]) {
+      storage.entry = {
+        name,
+        winner: ctx.props.always || randomCandidate(ctx.children)
+      };
     }
+    
+    const variations = ctx.slots();
+    const winner = storage.entry[name];
 
     return variations[winner][0];
   }
